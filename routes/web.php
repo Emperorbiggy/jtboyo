c75ede040865629a9bf7de086df25e83708700c5
Emperorbiggy <rifelinktechnology@gmail.com>
2025-07-18 13:16:04 +0100

    Home route added

--- routes/web.php
+++ routes/web.php
@@ -6,18 +6,21 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->name('dashboard');
+Route::get('/', function () {
+    return Inertia::render('Auth/Login');
+})->name('login');
 
 Route::get('/individual', function () {
     return Inertia::render('IndividualTaxPayer');
