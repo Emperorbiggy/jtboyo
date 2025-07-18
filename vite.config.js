b334483bbe07b25a03c592959de3e3c56a2323d0
Emperorbiggy <rifelinktechnology@gmail.com>
2025-07-18 14:33:36 +0100

    new updates

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-    base: '/app/public/build/', // 👈 ADD THIS
+    base: '/app/public/', 
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
